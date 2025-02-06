4b2f52f9ce6dd613d8aba41266e3e34bc6f6f5b6
meikled8665 <meikled8665@student.faytechcc.edu>
2025-02-06 14:20:36 -0500

    Add files via upload

---
+++ P1Lab1_MeikleDominic.py
@@ -0,0 +1,9 @@
+#Dominic Meikle
+#2/6/25
+#P1Lab1
+#Prints a greeting using your first and last name.
+
+fname = input("Enter your first name: ")
+lname = input("Enter your last name: ")
+
+print("Hello, " + str(fname) + " " + str(lname) + "! Welcome to CTI-110")
